8746316172475fc3494101be2dd2491fc5577258
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2017-10-30 12:29:32 -0400

    chapter 4 exercises

---
+++ five.py
@@ -0,0 +1,13 @@
+from test import testEqual
+
+def sumTo(n):
+    result = (n * (n + 1)) / 2
+    return result
+
+# Now lets see how well this works
+t = sumTo(0)
+print("The sum from 1 to 0 is",t)
+t = sumTo(10)
+print("The sum from 1 to 10 is",t)
+t = sumTo(5)
+print("The sum from 1 to 5 is",t)
